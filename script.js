ace5c00c90d598bae213749bb3e9b4366cfac7e9
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-04-20 09:08:17 -0400

    js updates

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("testing")
+const BASE_URL = "http://localhost:3000/dogs/"
+
+
+handleFetch()
+
+
+function handleFetch(){
+    const viewAdoptees = document.querySelector("#")
+    fetchButton.addEventListener('click', () => {
+
+        fetch(BASE_URL)
+            .then( (res) => res.json() )
+            .then( (dogData) => dogData.forEach(renderAdoptees) )
+    })
+}
+
+function renderAdoptees(dog){
+    const dogLi = document.createElement('li')
+
+    dogLi.innerText = dog.name 
+    dogLi.innerText = dog.breed
+    dogLi.innerHTML = dog.age 
+    dogLi.innerHTML = dog.image 
+
+    dogLi.className = "list-group-item"
+    
+    document.querySelector(".list-group").appendChild(dogLi)
+}
